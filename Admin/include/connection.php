e7b0ceaa8bddbc891d17a5153108a323874e3767
shayanahmad1999 <69293308+shayanahmad1999@users.noreply.github.com>
2022-10-13 15:54:43 +0500

    updated new feature

--- Admin/include/connection.php
+++ Admin/include/connection.php
@@ -2,7 +2,7 @@
 
     session_start();
     
-    $connect = mysqli_connect("localhost","root","","hospitalmanagementsystem");
+    $connect = mysqli_connect("localhost","root","","hospital_management_system");
     if (mysqli_connect_errno()) {
         echo "sorry data base is not connected". mysqli_connect_error();
     }
